0079e103e39e0a4351219bd6f52e7d2623f30f62
Ruinian Xu <rxu72@gatech.edu>
2021-12-08 11:21:06 -0500

    change version requirement of pytorch to 1.7.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'transformers>=4.6.0,<5.0.0',
         'tokenizers>=0.10.3',
         'tqdm',
-        'torch>=1.6.0',
+        'torch==1.7.0',
         'torchvision',
         'numpy',
         'scikit-learn',
